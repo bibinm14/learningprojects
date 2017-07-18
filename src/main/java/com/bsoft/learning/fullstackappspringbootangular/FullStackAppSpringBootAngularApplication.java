dd7dd3d6b23bd87a7f64128b388397d7b4c9b84d
mathbi03 <bibin.mathew.ap@nielsen.com>
2017-07-18 10:15:19 +0530

    techstartupprojects

---
+++ src/main/java/com/bsoft/learning/fullstackappspringbootangular/FullStackAppSpringBootAngularApplication.java
@@ -0,0 +1,12 @@
+package com.bsoft.learning.fullstackappspringbootangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAppSpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackAppSpringBootAngularApplication.class, args);
+	}
+}
